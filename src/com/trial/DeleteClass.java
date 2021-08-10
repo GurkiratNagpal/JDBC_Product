943457f7b9ea13b1c827396b0eb3c0621879c18c
= <=>
2021-08-10 14:44:29 +0530

    second commit

--- src/com/trial/DeleteClass.java
+++ src/com/trial/DeleteClass.java
@@ -5,14 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class InsertProduct {
-	
+public class DeleteClass {
+
 	public static void main(String[] args) {
-		
 		String url = "jdbc:mysql://localhost:3306/product";
 		String user = "gurkirat";
 		String pass = "gurkirat";
-		String insCom = "INSERT INTO prod VALUES(2, 'Pen', 10)";
+		String insCom = "Delete from prod where type ='PAPER'";
 		int i=0;
 		Connection cn =null;
 		Statement st = null;
@@ -20,14 +19,15 @@ public class InsertProduct {
 			cn = DriverManager.getConnection(url, user, pass);
 			st = cn.createStatement();
 			i=st.executeUpdate(insCom);
-			
+
 			System.out.println(i);
-			
-			
+
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 
 }
--- src/com/trial/SelectProduct.java
+++ src/com/trial/SelectProduct.java
@@ -2,26 +2,33 @@ package com.trial;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class InsertProduct {
-	
+public class SelectProduct {
 	public static void main(String[] args) {
-		
+
 		String url = "jdbc:mysql://localhost:3306/product";
 		String user = "gurkirat";
 		String pass = "gurkirat";
-		String insCom = "INSERT INTO prod VALUES(2, 'Pen', 10)";
+		String cmd = "SELECT * from prod";
 		int i=0;
 		Connection cn =null;
 		Statement st = null;
+		ResultSet rs = null;
 		try {
 			cn = DriverManager.getConnection(url, user, pass);
 			st = cn.createStatement();
-			i=st.executeUpdate(insCom);
-			
-			System.out.println(i);
+			rs =  st.executeQuery(cmd);
+			while(rs.next()) {
+				int id = rs.getInt("i");
+				String type = rs.getString("type");
+				int price = rs.getInt("pp");
+				System.out.println(id + "     " + type + "   "+ price);
+				
+			}
+
 			
 			
 		} catch (SQLException e) {
